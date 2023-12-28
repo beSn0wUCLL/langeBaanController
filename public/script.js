e2ecdfe54dd583a4e54ca72fb3202793747cfbb3
Robin Koolhaas <robin.koolhaas@student.ucll.be>
2023-12-28 13:23:48 +0100

    Initial Commit

---
+++ public/script.js
@@ -0,0 +1,81 @@
+console.log('Script.js loaded');
+
+const buttonToggleBackward = document.getElementById('buttonToggleBackward');
+const buttonToggleForward = document.getElementById('buttonToggleForward');
+const buttonStop = document.getElementById("buttonStop");
+
+const buttonHoldBackward = document.getElementById("buttonHoldBackward");
+const buttonHoldForward = document.getElementById("buttonHoldForward");
+
+buttonToggleBackward.addEventListener('click', () => {
+    motorBackward();
+});
+
+buttonToggleForward.addEventListener('click', () => {
+    motorForward();
+});
+
+buttonStop.addEventListener('click', () => {
+    motorStop();
+});
+
+//HOLD TO RUN BACKWARDS
+buttonHoldBackward.addEventListener("mousedown", () => {
+    motorBackward();
+});
+
+buttonHoldBackward.addEventListener("mouseup", () => {
+    motorStop();
+});
+
+buttonHoldBackward.addEventListener("touchstart", () => {
+    motorBackward();
+});
+
+buttonHoldBackward.addEventListener("touchend", () => {
+    motorStop();
+});
+
+//Prevents context menu from popping up
+buttonHoldBackward.addEventListener("contextmenu", event => {
+    event.preventDefault();
+});
+//HOLD TO RUN BACKWARDS END
+
+
+//HOLD TO RUN FORWARDS
+buttonHoldForward.addEventListener("mousedown", () => {
+    motorForward();
+});
+
+buttonHoldForward.addEventListener("mouseup", () => {
+    motorStop();
+});
+
+buttonHoldForward.addEventListener("touchstart", () => {
+    motorForward();
+});
+
+buttonHoldForward.addEventListener("touchend", () => {
+    motorStop();
+});
+
+buttonHoldForward.addEventListener("contextmenu", event => {
+    event.preventDefault();
+});
+//HOLD TO RUN FORWARDS END
+
+
+const motorBackward = () => {
+    motorStop();
+    console.log("Motor Backward");
+};
+
+const motorForward = () => {
+    motorStop();
+    console.log("Motor Forward");
+};
+
+const motorStop = () => {
+    console.log("Motor Stop");
+};
