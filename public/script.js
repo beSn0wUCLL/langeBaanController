b87a9891c010739754c983e2a45c79a2e56fade4
Robin Koolhaas <robin.koolhaas@student.ucll.be>
2024-01-03 16:05:01 +0100

    Added server callbacks

--- public/script.js
+++ public/script.js
@@ -8,74 +8,74 @@ const buttonHoldBackward = document.getElementById("buttonHoldBackward");
 const buttonHoldForward = document.getElementById("buttonHoldForward");
 
 buttonToggleBackward.addEventListener('click', () => {
-    motorBackward();
+    fetch('/backward', {method: 'POST'});
 });
 
 buttonToggleForward.addEventListener('click', () => {
-    motorForward();
+    fetch('/forward', {method: 'POST'});
 });
 
 buttonStop.addEventListener('click', () => {
-    motorStop();
+    fetch('/stop', {method: 'POST'});
 });
 
-//HOLD TO RUN BACKWARDS
-buttonHoldBackward.addEventListener("mousedown", () => {
-    motorBackward();
-});
+// //HOLD TO RUN BACKWARDS
+// buttonHoldBackward.addEventListener("mousedown", () => {
+//     motorBackward();
+// });
 
-buttonHoldBackward.addEventListener("mouseup", () => {
-    motorStop();
-});
+// buttonHoldBackward.addEventListener("mouseup", () => {
+//     motorStop();
+// });
 
-buttonHoldBackward.addEventListener("touchstart", () => {
-    motorBackward();
-});
+// buttonHoldBackward.addEventListener("touchstart", () => {
+//     motorBackward();
+// });
 
-buttonHoldBackward.addEventListener("touchend", () => {
-    motorStop();
-});
+// buttonHoldBackward.addEventListener("touchend", () => {
+//     motorStop();
+// });
 
-//Prevents context menu from popping up
-buttonHoldBackward.addEventListener("contextmenu", event => {
-    event.preventDefault();
-});
-//HOLD TO RUN BACKWARDS END
-
-
-//HOLD TO RUN FORWARDS
-buttonHoldForward.addEventListener("mousedown", () => {
-    motorForward();
-});
-
-buttonHoldForward.addEventListener("mouseup", () => {
-    motorStop();
-});
-
-buttonHoldForward.addEventListener("touchstart", () => {
-    motorForward();
-});
-
-buttonHoldForward.addEventListener("touchend", () => {
-    motorStop();
-});
-
-buttonHoldForward.addEventListener("contextmenu", event => {
-    event.preventDefault();
-});
-//HOLD TO RUN FORWARDS END
+// //Prevents context menu from popping up
+// buttonHoldBackward.addEventListener("contextmenu", event => {
+//     event.preventDefault();
+// });
+// //HOLD TO RUN BACKWARDS END
 
 
-const motorBackward = () => {
-    motorStop();
-    console.log("Motor Backward");
-};
+// //HOLD TO RUN FORWARDS
+// buttonHoldForward.addEventListener("mousedown", () => {
+//     motorForward();
+// });
 
-const motorForward = () => {
-    motorStop();
-    console.log("Motor Forward");
-};
+// buttonHoldForward.addEventListener("mouseup", () => {
+//     motorStop();
+// });
 
-const motorStop = () => {
-    console.log("Motor Stop");
-};
+// buttonHoldForward.addEventListener("touchstart", () => {
+//     motorForward();
+// });
+
+// buttonHoldForward.addEventListener("touchend", () => {
+//     motorStop();
+// });
+
+// buttonHoldForward.addEventListener("contextmenu", event => {
+//     event.preventDefault();
+// });
+// //HOLD TO RUN FORWARDS END
+
+
+// const motorBackward = () => {
+//     motorStop();
+//     console.log("Motor Backward");
+// };
+
+// const motorForward = () => {
+//     motorStop();
+//     console.log("Motor Forward");
+// };
+
+// const motorStop = () => {
+//     console.log("Motor Stop");
+// };
--- server.js
+++ server.js
@@ -10,3 +10,18 @@ app.get('/', (req, res) => {
 
 app.listen(PORT);
 console.log('Started Server at http://localhost:' + PORT);
+
+app.post('/backward', (req, res) => {
+    console.log('Clicked Fackward');
+    res.sendStatus(200);
+});
+
+app.post('/forward', (req, res) => {
+    console.log('Clicked Forward')
+    res.sendStatus(200);
+});
+
+app.post('/stop', (req, res) => {
+    console.log('Clicked Stop')
+    res.sendStatus(200);
+});
